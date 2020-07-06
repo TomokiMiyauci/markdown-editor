162fed1a7a35d465d5e49f33fb1803c87174fbe3
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-07-07 02:03:16 +0900

    :sparkles: (index.ts) Add prototype markdown editor

--- packages/client/src/pages/index.vue
+++ packages/client/src/pages/index.vue
@@ -1,5 +1,68 @@
-<template> <div></div></template>
+<template>
+  <v-container>
+    <v-card>
+      <v-toolbar flat dense outlined>Markdown editor</v-toolbar>
+      <v-row no-gutters>
+        <v-col
+          ref="div"
+          style="padding-left: 12px; margin-top: 10px; line-height: 1.75rem;"
+          cols="auto"
+        >
+          <v-slide-x-reverse-transition :group="true">
+            <div v-for="i in editorRows" :key="i">{{ i }}</div>
+          </v-slide-x-reverse-transition>
+        </v-col>
+        <v-col class="edit" cols="5">
+          <v-textarea
+            v-model="text"
+            hide-details
+            flat
+            solo
+            type="text"
+            auto-grow
+          ></v-textarea>
+        </v-col>
+        <v-col class="result markdown-body" cols="6" v-html="$md.render(text)"
+          >{{ text }}
+        </v-col>
+      </v-row>
+    </v-card>
+  </v-container>
+</template>
 
-<script>
-export default {}
+<script lang="ts">
+import { defineComponent, ref, onMounted, computed } from 'nuxt-composition-api'
+export default defineComponent({
+  setup() {
+    const text = ref('')
+    const div = ref<HTMLDivElement>()
+
+    onMounted(() => {
+      console.log(
+        div.value.clientHeight,
+        div.value.offsetHeight,
+        div.value.scrollHeight
+      )
+    })
+
+    const editorRows = computed(() => {
+      return Math.floor(text.value.split('\n').length)
+    })
+
+    return { text, div, editorRows }
+  },
+})
 </script>
+
+<style scoped lang="scss">
+.edit {
+  border-bottom-left-radius: 30px;
+}
+.result {
+  padding-left: 12px;
+  margin-top: 10px;
+  line-height: 1.75rem;
+  border-left: 1px solid rgba(0, 0, 0, 0.12);
+  border-bottom-right-radius: 30px;
+}
+</style>
