e660babcb46b749de35a5e956626d4e73d3553b9
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-07-09 01:15:25 +0900

    :recycle: Use real editor

---
+++ packages/client/src/components/organisms/Wrapper.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-container class="mt-16">
+    <v-row
+      no-gutters
+      align="center"
+      style="
+        border: 1px solid;
+        border-top-left-radius: 5px;
+        border-top-right-radius: 5px;
+      "
+      tag="header"
+    >
+      <v-col
+        class="pa-3"
+        style="background-color: rgba(38, 50, 56, 0.8);"
+        cols="6"
+        ><v-btn icon color="white"
+          ><v-icon>{{ mdiFileImport }}</v-icon></v-btn
+        ></v-col
+      >
+      <v-col style="background-color: grey;" cols="6"></v-col>
+    </v-row>
+    <v-row no-gutters>
+      <v-col cols="6">
+        <the-editor @input="text = $event" />
+      </v-col>
+      <v-col cols="6">
+        <markdown-preview :text="text" />
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script lang="ts">
+import { mdiFileImport } from '@mdi/js'
+import { defineComponent, ref } from 'nuxt-composition-api'
+export default defineComponent({
+  setup() {
+    const text = ref('')
+    return {
+      text,
+      mdiFileImport,
+    }
+  },
+})
+</script>
--- packages/client/src/pages/index.vue
+++ packages/client/src/pages/index.vue
@@ -1,83 +1,8 @@
 <template>
-  <v-container>
-    <v-card>
-      <v-toolbar flat dense outlined>Markdown editor</v-toolbar>
-      <v-row no-gutters>
-        <v-col
-          ref="div"
-          style="padding-left: 12px; margin-top: 10px; line-height: 1.75rem;"
-          cols="auto"
-        >
-          <v-slide-x-reverse-transition :group="true">
-            <div v-for="i in editorRows" :key="i">{{ i }}</div>
-          </v-slide-x-reverse-transition>
-        </v-col>
-        <v-col class="edit" cols="5">
-          <v-textarea
-            v-model="text"
-            hide-details
-            flat
-            solo
-            type="text"
-            auto-grow
-          ></v-textarea>
-        </v-col>
-        <v-col
-          v-if="editCount"
-          class="result markdown-body"
-          cols="6"
-          v-html="$md.render(text)"
-          >{{ text }}
-        </v-col>
-        <v-col v-else class="fill-height result" cols="6">
-          <v-row justify="center" class="fill-height" align="center">
-            <v-col cols="auto">
-              sfaf
-            </v-col>
-          </v-row>
-        </v-col>
-      </v-row>
-    </v-card>
-  </v-container>
+  <wrapper />
 </template>
 
 <script lang="ts">
-import { defineComponent, ref, onMounted, computed } from 'nuxt-composition-api'
-export default defineComponent({
-  setup() {
-    const text = ref('')
-    const div = ref<HTMLDivElement>()
-
-    onMounted(() => {
-      console.log(
-        div.value.clientHeight,
-        div.value.offsetHeight,
-        div.value.scrollHeight
-      )
-    })
-
-    const editorRows = computed(() => {
-      return Math.floor(text.value.split('\n').length)
-    })
-
-    const editCount = computed(() => {
-      return !!text.value.split('').length
-    })
-
-    return { text, div, editorRows, editCount }
-  },
-})
+import { defineComponent } from 'nuxt-composition-api'
+export default defineComponent({})
 </script>
-
-<style scoped lang="scss">
-.edit {
-  border-bottom-left-radius: 30px;
-}
-.result {
-  padding-left: 12px;
-  margin-top: 10px;
-  line-height: 1.75rem;
-  border-left: 1px solid rgba(0, 0, 0, 0.12);
-  border-bottom-right-radius: 30px;
-}
-</style>
