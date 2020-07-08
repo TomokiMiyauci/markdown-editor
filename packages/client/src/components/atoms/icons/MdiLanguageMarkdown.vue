4b02e1e52436259e538f25f756a93c3d8d87bd35
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-07-09 01:13:28 +0900

    :sparkles: (MdiLanguageMarkdown.vue) Add mdi-language-markdown

---
+++ packages/client/src/components/atoms/icons/MdiLanguageMarkdown.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-icon v-bind="$attrs">{{ mdiLanguageMarkdown }}</v-icon>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'nuxt-composition-api'
+import { mdiLanguageMarkdown } from '@mdi/js'
+export default defineComponent({
+  setup() {
+    return { mdiLanguageMarkdown }
+  },
+})
+</script>
