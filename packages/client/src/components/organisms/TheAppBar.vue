ecc19c39b5d767913acf7c66ee29c18f5bf212d3
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-07-09 01:15:00 +0900

    :sparkles: Add app bar

---
+++ packages/client/src/components/organisms/TheAppBar.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-app-bar app>
+    <v-toolbar-title>
+      <mdi-language-markdown left />
+      Markdown Editor
+    </v-toolbar-title>
+  </v-app-bar>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'nuxt-composition-api'
+
+export default defineComponent({})
+</script>
