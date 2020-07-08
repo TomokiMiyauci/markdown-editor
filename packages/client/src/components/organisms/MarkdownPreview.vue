8ebb8ba14b2010511315581416197fdbf5214268
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-07-09 01:14:02 +0900

    :sparkles: (MarkdownPreview.vue) Add markdown preview component

---
+++ packages/client/src/components/organisms/MarkdownPreview.vue
@@ -0,0 +1,36 @@
+<template>
+  <div
+    class="markdown-body"
+    :style="{ minHeight }"
+    :class="$style.base"
+    v-html="$md.render(text)"
+  ></div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'nuxt-composition-api'
+export default defineComponent({
+  props: {
+    minHeight: {
+      type: [String, Number],
+      default: '300px',
+    },
+
+    text: {
+      type: String,
+      default: '',
+    },
+  },
+})
+</script>
+
+<style lang="scss" module>
+.base {
+  padding: 20px;
+  background-color: white;
+  border-top: 1px solid black;
+  border-right: 1px solid black;
+  border-bottom: 1px solid black;
+  border-bottom-right-radius: 5px;
+}
+</style>
