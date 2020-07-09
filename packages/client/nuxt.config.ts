6a8df7bfeaaec3f8164d7afcc12f92cf6c9f43e6
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-07-09 23:41:53 +0900

    :zap: (nuxt.config.ts) Apply super slim

--- packages/client/nuxt.config.ts
+++ packages/client/nuxt.config.ts
@@ -138,12 +138,26 @@ const config: Configuration = {
   markdownit: {
     injected: true,
   },
+
+  features: {
+    store: false,
+    middleware: false,
+    transitions: false,
+    deprecations: false,
+    validate: false,
+    asyncData: false,
+    fetch: false,
+    clientOnline: false,
+    clientPrefetch: false,
+    clientUseUrl: false,
+    componentAliases: false,
+    componentClientOnly: false,
+  },
   /*
    ** Build configuration
    ** See https://nuxtjs.org/api/configuration-build/
    */
   build: {
-    hardSource: true,
     extractCSS: process.env.NODE_ENV === 'production',
     terser: {
       terserOptions: {
