f41afa6ac5173e320839291acb4192e7fd8584aa
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-07-13 22:06:08 +0900

    :sparkles: (EmojiPicker.vue) Add emoji picker wrapper

---
+++ packages/client/src/components/molecules/EmojiPicker.vue
@@ -0,0 +1,27 @@
+<template>
+  <picker set="facebook" :data="emojiIndex" />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'nuxt-composition-api'
+
+import { Picker, EmojiIndex } from 'emoji-mart-vue-fast'
+import data from 'emoji-mart-vue-fast/data/facebook.json'
+const emojiIndex = new EmojiIndex(data)
+
+export default defineComponent({
+  components: {
+    Picker,
+  },
+
+  setup() {
+    return {
+      emojiIndex,
+    }
+  },
+})
+</script>
+
+<style>
+@import 'emoji-mart-vue-fast/css/emoji-mart.css';
+</style>
