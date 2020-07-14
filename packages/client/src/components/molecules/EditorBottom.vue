00c32fd07c95ccd0e0b64cb74cf9d6d5a42fb815
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-07-15 02:34:52 +0900

    :sparkles: (EditorBottom.vue) Add bottom of editor

---
+++ packages/client/src/components/molecules/EditorBottom.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-row :class="$style.base" justify="end" no-gutters>
+    <v-col class="pa-1 pr-5" cols="auto">
+      Ln , Col UTF-8
+    </v-col>
+  </v-row>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'nuxt-composition-api'
+export default defineComponent({})
+</script>
+
+<style lang="scss" module>
+$color: rgb(38, 50, 56);
+.base {
+  color: rgb(82, 102, 120);
+  background-color: $color;
+}
+</style>
