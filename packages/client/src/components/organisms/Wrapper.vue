3e7708b8f9634e1db428d4a31f46ecbd7d5a06b5
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-07-15 02:39:02 +0900

    :sparkles: (Wrapper.vue) Add inserting emoji

--- packages/client/src/components/organisms/Wrapper.vue
+++ packages/client/src/components/organisms/Wrapper.vue
@@ -19,27 +19,36 @@
         >
         <v-menu
           v-model="menu"
-          transition="slide-x-transition"
           :close-on-content-click="false"
+          transition="slide-x-transition"
         >
           <template #activator="{on: menu, attrs}">
             <v-tooltip bottom>
               <template #activator="{on: tooltip }">
-                <v-btn v-bind="attrs" icon v-on="{ ...tooltip, ...menu }">
+                <v-btn
+                  v-bind="attrs"
+                  @click="onClick"
+                  v-on="{ ...tooltip, ...menu }"
+                  icon
+                >
                   <v-icon> {{ mdiEmoticon }}</v-icon>
                 </v-btn>
               </template>
               <span>emoji</span>
             </v-tooltip>
           </template>
-          <lazy-emoji-picker @select="menu = false" />
+          <lazy-emoji-picker @select="onSelect" />
         </v-menu>
       </v-col>
       <v-col style="background-color: grey;" cols="6"></v-col>
     </v-row>
-    <v-row no-gutters>
+    <v-row
+      no-gutters
+      style="max-height: 700px; border: 1px solid grey; border-radius: 5px;"
+    >
       <v-col cols="6">
         <the-editor @input="text = $event" />
+        <editor-bottom />
       </v-col>
       <v-col cols="6">
         <markdown-preview :text="text" />
@@ -51,15 +60,35 @@
 <script lang="ts">
 import { mdiFileImport, mdiEmoticon } from '@mdi/js'
 import { defineComponent, ref } from 'nuxt-composition-api'
+import { editor } from '@/store'
 export default defineComponent({
   setup() {
     const text = ref('')
     const menu = ref(false)
+    const onSelect = (a: any) => {
+      try {
+        console.log(a.native)
+        editor.range.insertNode(document.createTextNode(a.native))
+
+        menu.value = false
+      } catch (e) {
+        console.log(e)
+      }
+    }
+
+    const onClick = () => {
+      const selection = document.getSelection()
+
+      editor.setRange(selection.getRangeAt(0))
+      console.log(selection)
+    }
     return {
+      onClick,
       menu,
       text,
       mdiFileImport,
       mdiEmoticon,
+      onSelect,
     }
   },
 })
